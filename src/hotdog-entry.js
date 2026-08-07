fed6d87b5f93668ca160e9e9351d190dccff3883
ControlMC-unstable <cadenjohnson739@gmail.com>
2026-08-07 02:49:28 -0500

    Delete src directory

--- src/hotdog-entry.js
+++
@@ -1,5 +0,0 @@
-import { mountHotdog } from './hotdog.js';
-
-// Exposed as a plain global so the pages can use a normal <script> tag.
-// (ES modules don't load from file:// URLs, which broke local previews.)
-window.mountHotdog = mountHotdog;
